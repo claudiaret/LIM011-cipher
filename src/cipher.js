00a8fbe4de0cb5f67748820a2f6f89b31d648225
claudiaret <claudiaretuertoa@gmail.com>
2019-10-10 12:31:50 -0500

    subiendo1

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,25 @@
 window.cipher = {
-  // Acá escribe tu código.
-};
+  encode:function cifrando(off1, cifrar) {
+    let ascii;
+      let newCifra = "";
+      let unew;
+      for ( let i=0; i<cifrar.length; i++ ) {
+        ascii = cifrar.charCodeAt(i);
+        if ( ascii >= 65 && ascii <= 90) {
+           unew = (( ascii - 65 + off1 ) % 26) + 65;}
+        newCifra += String.fromCharCode(unew);
+      }
+      return newCifra;
+    },
+  decode: function descifrando(off2,descifrar) {
+    let asciiD;
+      let newDescifra = "";
+      let ucifra;
+      for ( let i=0; i<descifrar.length; i++ ) {
+        asciiD = descifrar.charCodeAt(i);
+        if ( asciiD >= 65 && asciiD <= 90) {
+           ucifra = (( asciiD - 90 - off2 ) % 26) + 90;}
+       newDescifra += String.fromCharCode(ucifra);
+      }
+      return newDescifra;},
+    };
---
+++ src/index.js
@@ -0,0 +1,39 @@
+//const btn = document.getElementById('btn');
+//btn.addEventListener('click', 2do parámetro);
+//btn.addEventListener('click', () => { alert(`Hola! Mucho gusto en conocerte`);});
+//? ? ()=> { } ??
+const cifrarUser= document.getElementById("pagecifrar");
+cifrarUser.addEventListener("click",function cifrarUser() {document.getElementById("page1").style.display="none"
+document.getElementById("page2").style.display="block";})
+
+const descifraUser= document.getElementById("pagedescifrar");
+descifraUser.addEventListener("click",function descifraUser(){document.getElementById("page1").style.display="none"
+document.getElementById("page3").style.display="block";})
+
+const regresarInicio= document.getElementById("backto-page1");
+regresarInicio.addEventListener("click",function regresarInicio(){document.getElementById("page1").style.display="block"
+document.getElementById("page2").style.display="none"
+document.getElementById("page3").style.display="none"})
+
+
+const regresarInicio2= document.getElementById("b-pg1");
+regresarInicio2.addEventListener("click",function regresarInicio2(){document.getElementById("page1").style.display="block"
+document.getElementById("page2").style.display="none"
+document.getElementById("page3").style.display="none"})
+
+
+const resultadoCifrado = document.getElementById("usuario-cifrado");
+const ejecutaCifrado = document.getElementById("EJECUTA-CIFRADO");
+ejecutaCifrado.addEventListener("click", ()=>{
+  const offc = document.getElementById("offset-cifrado").value;
+  const ingresaUserNew = document.getElementById("ingresa-usuarionuevo").value;
+  resultadoCifrado.innerHTML = cipher.encode(parseInt(offc),ingresaUserNew);
+})
+
+const resultadoDescifrado = document.getElementById("usuario-real");
+const ejecutaDescifrado = document.getElementById("EJECUTA-DESCIFRADO");
+ejecutaDescifrado.addEventListener("click", ()=>{
+const offd = document.getElementById("offset-descifrado").value;
+const ingresaUserC = document.getElementById("ingresa-usuariocifrado").value;
+resultadoDescifrado.innerHTML = cipher.decode(parseInt(offd),ingresaUserC);
+})
